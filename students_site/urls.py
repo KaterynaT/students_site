f54e27171a9f86cc73db099b8264e4142f0e9184
KaterynaT <tkachkv@i.ua>
2016-11-18 13:29:59 +0200

    add stud site

--- students_site/urls.py
+++ students_site/urls.py
@@ -17,9 +17,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^students/', include('students.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', RedirectView.as_view(url='/students/')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
